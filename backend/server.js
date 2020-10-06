ad87d3fd711df95862ccc90c3ac753cafb8c3ddb
XanderUZZZER <xanderuzzzer@gmail.com>
2020-10-06 15:23:53 +0300

    Nodemon, concurrently, .env and esmodules

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+
+dotenv.config();
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('API is running...')
+  res.send('API is running....')
 });
 
 app.get('/api/products/', (req, res) => {
@@ -18,5 +21,5 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-const PORT = 5000;
-app.listen(PORT, console.log(`Server is runnind on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server is running in ${process.NODE_ENV} mode on port ${PORT}`));
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ const HomeScreen = () => {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios.get('/api/pruducts');
+      const { data } = await axios.get('/api/products');
 
       setProducts(data);
     }
