dad606299a094a3fe278610818d268c8a266476d
XanderUZZZER <xanderuzzzer@gmail.com>
2020-10-06 16:44:23 +0300

    Connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -11,15 +15,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/products/', (req, res) => {
-  console.log('All products requested');
+  console.log('All products requested'.bgBrightRed);
   res.json(products)
 });
 
 app.get('/api/products/:id', (req, res) => {
-  console.log(`Product with id: ${req.params.id} requested`);
+  console.log(`Product with id: ${req.params.id} requested`.bgRed.green.bold);
   const product = products.find(p => p._id === req.params.id);
   res.json(product);
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server is running in ${process.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
