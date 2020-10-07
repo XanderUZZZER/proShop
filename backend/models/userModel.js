46ae6c330c5ca9efb267d59e84f19b9cf7cc331d
XanderUZZZER <xanderuzzzer@gmail.com>
2020-10-07 22:25:55 +0300

    Auth middleware, User registration & pass hashing

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
   {
@@ -24,8 +25,20 @@ const userSchema = mongoose.Schema(
   {
     timestamps: true
   }
-);
+)
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
 
-export default User;
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
