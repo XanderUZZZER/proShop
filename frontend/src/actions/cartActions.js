de355649d6859f7c42275dd2a537874151f5cf53
XanderUZZZER <xanderuzzzer@gmail.com>
2020-10-08 17:03:54 +0300

    Shipping screen & save address

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import {
   CART_ADD_ITEM,
-  CART_REMOVE_ITEM
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS
 } from '../constants/cartConstants';
 
 
@@ -31,4 +32,13 @@ export const removeFromCart = (id) => (dispatch, getState) => {
   })
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: data,
+  })
+
+  localStorage.setItem('shippingAddress', JSON.stringify(data))
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -24,17 +24,24 @@ const reducer = combineReducers({
 
 });
 
-const cartItemsFromLocalStorage = localStorage.getItem('cartItems')
+const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
-const userInfoFromLocalStorage = localStorage.getItem('userInfo')
+const userInfoFromStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {}
+
 const initialState = {
-  cart: { cartItems: cartItemsFromLocalStorage },
-  userLogin: { userInfo: userInfoFromLocalStorage }
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
+  userLogin: { userInfo: userInfoFromStorage }
 };
 const middleware = [thunk];
 
