286e2619d895f4a19a72185991930daeccc10e46
XanderUZZZER <xanderuzzzer@gmail.com>
2020-10-08 23:12:51 +0300

    User details and orders reset

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -10,11 +10,14 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
+  USER_DETAILS_RESET,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_RESET
 } from '../constants/userConstants'
+
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -54,6 +57,8 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => dispatch => {
   localStorage.removeItem('userInfo')
   dispatch({ type: USER_LOGOUT })
+  dispatch({ type: USER_DETAILS_RESET })
+  dispatch({ type: ORDER_LIST_MY_RESET })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
