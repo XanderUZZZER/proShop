1032a872a7a6bb807649e9915476ec560f223956
XanderUZZZER <xanderuzzzer@gmail.com>
2020-10-13 23:20:14 +0300

    Image upload

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,7 +9,7 @@ import {
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 router
   .route('/:id')
   .get(getProductById)
