469d29c41a6c2f00e2b1d1fb56e41bf6c6ce155d
XanderUZZZZER <xanderuzzzer@gmail.com>
2020-10-26 21:27:11 +0200

    productReviewCreateReducer added to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,8 @@ import {
   productDetailsReducer,
   productDeleteReducer,
   productCreateReducer,
-  productUpdateReducer
+  productUpdateReducer,
+  productReviewCreateReducer
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers'
 import {
@@ -33,6 +34,7 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
