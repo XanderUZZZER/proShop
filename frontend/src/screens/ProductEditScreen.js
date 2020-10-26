402de4f7561255153bcc57edb7dd256b33a815ac
XanderUZZZZER <xanderuzzzer@gmail.com>
2020-10-26 22:59:07 +0200

    Product pagination

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -8,7 +8,6 @@ import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
 import { listProductDetails, updateProduct } from '../actions/productActions'
 import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
-import { productDeleteReducer } from '../reducers/productReducers'
 
 const ProductEditScreen = ({ match, history }) => {
   const productId = match.params.id
